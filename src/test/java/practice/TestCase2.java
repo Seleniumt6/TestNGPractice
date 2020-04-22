ea6f15629332809e1823265309f228087e7e236d
babureddy <babureddy.kota@gmail.com>
2020-04-23 03:24:12 +0530

    Fifth

--- src/test/java/practice/TestCase2.java
+++ src/test/java/practice/TestCase2.java
@@ -2,7 +2,6 @@ package practice;
 
 import org.testng.Assert;
 import org.testng.ITestListener;
-//import org.testng.ITestListener;
 import org.testng.annotations.Test;
 
 public class TestCase2 implements ITestListener{
@@ -18,7 +17,7 @@ public class TestCase2 implements ITestListener{
 		 * System.out.println("Test case Passed"); }else {
 		 * System.out.println("Test Case Failed"); }
 		 *
-		*
+		*;;
 		 */
 		Assert.assertEquals(webTitle, xltitle);
 		
